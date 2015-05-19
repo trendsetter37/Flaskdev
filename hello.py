25036c33a2831e3d906a23971c8f3d864da8e633
Javis Sullivan <javissullivan@gmail.com>
2015-05-19 14:41:36 -0400

    added static folder for css and js

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import sys
 
 app = Flask(__name__)
 
@@ -7,4 +8,7 @@ def hello_world():
 	return "Hello World"
 
 if __name__ == '__main__':
-	app.run()
+	if len(sys.argv) > 1:
+		app.run(host=sys.argv[1])
+	else:
+		app.run(host='0.0.0.0', debug=True) # for testing purposes
