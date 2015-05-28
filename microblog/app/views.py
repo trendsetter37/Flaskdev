295764469f0bfb179b2a31c156d7161830699465
Javis Sullivan <javissullivan@gmail.com>
2015-05-28 08:28:28 -0400

    started flask mega-tutorial

---
+++ microblog/app/views.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+	user = {'nickname' : 'Migel'} #  Fake
+	posts = [# fake array of posts
+		{
+			'author' : { 'nickname' : 'John' },
+			'body'	 : 'Beautiful day in Portland'
+		},
+		{
+			'author' : { 'nickname' : 'Susan' },
+			'body'	 : 'The Avengers movie was so cool!'
+		}
+	]
+
+	return render_template('index.html',
+							user=user,
+							title='Home',
+							posts=posts)
