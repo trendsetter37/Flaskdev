db594f925921091a1592c22336dcabe918f4d427
Javis Sullivan <javissullivan@gmail.com>
2015-06-04 15:49:11 -0400

    more microblog stuff

---
+++ config.py
@@ -0,0 +1,3 @@
+
+SQLALCHEMY_DATABASE_URI = "postgresql://flaskruser:flaskrpassword@localhost/flaskr"
+
--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
-from app import views
+app.config.from_object('config') # file named config next to app package
+db = SQLAlchemy(app)
+
+from app import views, models
