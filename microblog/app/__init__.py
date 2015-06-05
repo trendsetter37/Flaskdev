817586f2724ece910cf88384887e2ae06630b152
Javis Sullivan <javissullivan@gmail.com>
2015-06-04 22:21:13 -0400

    start on login stuff

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import openid
+from config import basedir
 
-
+lm = LoginManager
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 app = Flask(__name__)
 app.config.from_object('config') # file named config next to app package
--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, get
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from .forms import LoginForm
+from .models import User
 
 @app.route('/')
 @app.route('/index')
@@ -23,13 +25,20 @@ def index():
 							posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+	if g.user is not None and g.user.is_authenticated():
+		return redirect(url_for('index'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		flash('Login requested for OpenID={0}, remember_me={1}'\
-			.format(form.openid.data, str(form.remember_me.data)))
-		return redirect('/index')
+		session['remember_me'] = form.remember_me.data
+		return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
 	return render_template('login.html',
 							title='Sign In',
 							form=form,
 							providers=app.config['OPENID_PROVIDERS'])
+
+@lm.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
