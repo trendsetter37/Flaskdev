bc12c40a262e900a575fd299e55213acd14c4cd4
Javis Sullivan <javissullivan@gmail.com>
2015-06-06 10:01:30 -0400

    not empty

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -4,13 +4,15 @@ from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir
 
+
+app = Flask(__name__)
+app.config.from_object('config') # file named config next to app package
+db = SQLAlchemy(app)
+
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
-app = Flask(__name__)
-app.config.from_object('config') # file named config next to app package
-db = SQLAlchemy(app)
 
 from app import views, models
